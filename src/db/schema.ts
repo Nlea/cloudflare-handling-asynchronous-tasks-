d1d24dfa0698e27b1c8da87ab668847e60ed467d
Brett Beutell <brbeut@gmail.com>
2024-11-01 18:05:04 -0700

    Add biome

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,14 @@
-import { pgTable, serial, text, pgEnum, timestamp} from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
-export const  distanceEnum = pgEnum('distance', ['10', 'half', 'full']);
+export const distanceEnum = pgEnum("distance", ["10", "half", "full"]);
 
-export const runners = pgTable('runners', {
-  id: serial('id').primaryKey(),
-  firstName: text('first_name'),
-  lastName: text('last_name'),
-  address: text('address'),
-  distance: distanceEnum('distance'),
-  email: text('email').unique(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+export const runners = pgTable("runners", {
+  id: serial("id").primaryKey(),
+  firstName: text("first_name"),
+  lastName: text("last_name"),
+  address: text("address"),
+  distance: distanceEnum("distance"),
+  email: text("email").unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
--- src/email_template.tsx
+++ src/email_template.tsx
@@ -1,15 +1,17 @@
-import type * as React from 'react';
+import type * as React from "react";
 
 interface EmailTemplateProps {
   firstName: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  firstName
+  firstName,
 }) => (
   <div>
     <h1>Hello, {firstName}!</h1>
-    <div>Thanks for signing up to the race! You will receive more information closer to the date. 
+    <div>
+      Thanks for signing up to the race! You will receive more information
+      closer to the date.
     </div>
   </div>
 );
